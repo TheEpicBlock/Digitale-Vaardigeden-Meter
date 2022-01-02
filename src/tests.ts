c6662756bed8cec99bcc050a3fab2e6b001096c8
TheEpicBlock <git.teb@theepicblock.nl>
2022-01-02 18:50:14 +0100

    Implement Gmail & GSearch stuff

--- src/tests.ts
+++ src/tests.ts
@@ -2,6 +2,8 @@ import * as GS from './globalstate'
 import test0 from './tests/0.html'
 import test1 from './tests/1.html'
 import test2 from './tests/2.html'
+import test3 from './tests/3.html'
+import testend from './tests/end.html'
 
 export interface Test {
     id: number
@@ -14,7 +16,7 @@ export function getById(id: number): Test {
 }
 
 export function getFirstTest(): Test {
-    return allTests[0];
+    return allTests[3];
 }
 
 function htmlFromFile(file: string) {
@@ -29,13 +31,24 @@ function htmlFromFile(file: string) {
     }
 }
 
+function testElementValue(id: string, expected: string, ignoreTrailingDot: boolean): boolean {
+    var elem = document.getElementById(id);
+    if (elem instanceof HTMLInputElement || elem instanceof HTMLTextAreaElement) {
+        var content = elem.value.trim().toLowerCase();
+        if (ignoreTrailingDot && content.endsWith(".")) {
+            content = content.substr(0, content.length-1);
+        }
+        return content === expected;
+    }
+}
+
 const allTests: Array<Test> = [
-    {
+    { // Simple click test
         id: 0,
         onHtmlMessage: GS.toNextTest,
         getHtml: htmlFromFile(test0),
     },
-    {
+    { // Open chrome
         id: 1,
         onHtmlMessage: function(message) {
             if (message == "continue") {
@@ -50,10 +63,29 @@ const allTests: Array<Test> = [
         },
         getHtml: htmlFromFile(test1),
     },
-    {
+    { // Gmail
         id: 2,
-        onHtmlMessage: GS.toNextTest,
+        onHtmlMessage: function(message) {
+            var rec = testElementValue("reciever", "fpcvanmesdag@vanmesdag.nl", false);
+            var subj = testElementValue("subject", "hallo", true);
+            var cont = testElementValue("content", "dit is mijn bericht", true);
+            if (rec && subj && cont) {
+                GS.toNextTest();
+            } else {
+                // TODO
+            }
+        },
         getHtml: htmlFromFile(test2),
+    },
+    { // Google
+        id: 3,
+        onHtmlMessage: GS.toNextTest,
+        getHtml: htmlFromFile(test3),
+    },
+    {
+        id: 4,
+        onHtmlMessage: GS.toNextTest,
+        getHtml: htmlFromFile(testend),
     }
 ];
 
